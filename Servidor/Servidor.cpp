5d1ad76f5db5d7238e5927cf8ee5ec92fa1d73d1
MatheusMorata <matlucar09@hotmail.com>
2026-08-24 15:17:40 -0300

    fix bug

--- Servidor/Servidor.cpp
+++ Servidor/Servidor.cpp
@@ -15,7 +15,7 @@ int main(){
     serverAddress.sin_family = AF_INET;
     serverAddress.sin_port = htons(8080);
     serverAddress.sin_addr.s_addr = INADDR_ANY;
-    std::cout <<  "Servidor iniciando";
+    std::cout <<  "Servidor iniciando" << std::end1;
 
     bind(serverSocket, (struct sockaddr*)&serverAddress, sizeof(serverAddress));
 
