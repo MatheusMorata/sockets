79b97231c67bb680cd0f06a28dfabfcf9f8d5d9d
MatheusMorata <matlucar09@hotmail.com>
2026-08-24 10:35:12 -0300

    fix

--- Cliente/Cliente.lua
+++ Cliente/Cliente.lua
@@ -2,13 +2,12 @@ local socket = require 'socket'
 
 local address = '127.0.0.1'
 local port = 8080
-
 local client = socket.tcp()
 
-local ok, err = client:connect(address, port)
+local ok = client:connect(address, port)
 
-if not ok then
-    print("Erro ao conectar:", err)
+if ok then
+    print('Conectado ao servidor')
 else
-    print("Conectado!")
+    print('Não conectado')
 end
