dc36d69220e78f62f12f2c96a4975a4c0b0bde69
MatheusMorata <matlucar09@hotmail.com>
2026-08-24 09:56:26 -0300

    feat: Alterações em Servidor.cpp

--- Servidor/Servidor.cpp
+++ Servidor/Servidor.cpp
@@ -1,5 +1,24 @@
 #include <iostream>
 
 int main(){
+    sockaddr_in serverAddress;
+    int serverSocket, clientSocket;
+
+    // Criando o socket
+    serverSocket = socket(AF_INET, SOCK_STREAM, 0);
+    
+    // Definindo endereço
+    serverAddress.sin_family = AF_INET;
+    serverAddress.sin_port = htons(8080);
+    serverAddress.sin_addr.s_addr = INADDR_ANY;
+    
+    bind(serverSocket, (struct sockaddr*)&serverAddress, sizeof(serverAddress));
+
+    listen(serverSocket, 5);
+
+    clientSocket = accept(serverSocket, nullptr, nullptr);
+
+    close(serverSocket);
+
     return 0;
 }
