a1c3a314f81d95e24cbba1052217fc8d5665d549
MatheusMorata <matlucar09@hotmail.com>
2026-08-24 10:41:52 -0300

    feat: Adicionando envio de mensagem

--- Cliente/Cliente.lua
+++ Cliente/Cliente.lua
@@ -2,12 +2,14 @@ local socket = require 'socket'
 
 local address = '127.0.0.1'
 local port = 8080
+local pacote = 'Mensagem'
 local client = socket.tcp()
 
 local ok = client:connect(address, port)
 
 if ok then
     print('Conectado ao servidor')
+    client:send(pacote)
 else
     print('Não conectado')
 end
