14b6a5e4f72c399347f6fd67a10fbe23e14b6f6e
MatheusMorata <matlucar09@hotmail.com>
2026-08-24 21:50:56 -0300

    feat: Tratamento de erros

--- Cliente/Cliente.lua
+++ Cliente/Cliente.lua
@@ -11,33 +11,61 @@ print('Conectando ao servidor...')
 
 if ok then
     print('Conectado: ' .. address .. ':' .. port)
+
     while true do
         print('=============================')
-        print('[1] - Inteiro ' .. '\n[2] - Caractere' .. '\n[3] - Cadeia de carecteres')
+        print('[1] - Inteiro')
+        print('[2] - Caractere')
+        print('[3] - Cadeia de caracteres')
         print('=============================')
-        opcao = io.read()
-        
-        if opcao == '1' then
-            pacote.tipo = 'int'
-            io.write('Valor: ')
-            pacote.val = io.read()
-        elseif opcao == '2' then
-            pacote.tipo = 'char'
-            io.write('Valor: ')
-            pacote.val = io.read()
-        elseif opcao == '3' then
-            pacote.tipo = 'string'
-            io.write('Valor: ')
-            pacote.val = io.read()
-        else 
-            print('opcao invalida...')
-        end 
 
-        local resultado = string.format(
-            '{"tipo":"%s","val":"%s"}',
-            pacote.tipo, pacote.val)
-        
-        cliente:send(resultado)
+        opcao = io.read()
+
+        local valido = false
+
+        if opcao == '1' then
+            io.write('Valor: ')
+            local valor = io.read()
+
+            if tonumber(valor) and valor:match('^-?%d+$') then
+                pacote.tipo = 'int'
+                pacote.val = valor
+                valido = true
+            else
+                print('Valor inválido! Digite apenas um número inteiro.')
+            end
+
+        elseif opcao == '2' then
+            io.write('Valor: ')
+            local valor = io.read()
+
+            if #valor == 1 then
+                pacote.tipo = 'char'
+                pacote.val = valor
+                valido = true
+            else
+                print('Valor inválido! Digite apenas um caractere.')
+            end
+
+        elseif opcao == '3' then
+            io.write('Valor: ')
+            pacote.tipo = 'string'
+            pacote.val = io.read()
+            valido = true
+
+        else
+            print('Opção inválida!')
+        end
+
+        if valido then
+            local resultado = string.format(
+                '{"tipo":"%s","val":"%s"}',
+                pacote.tipo,
+                pacote.val
+            )
+
+            cliente:send(resultado)
+        end
     end
 
     cliente:close()
