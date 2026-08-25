53fe8a30c347413093a87e640b551f78431d2ca8
MatheusMorata <matlucar09@hotmail.com>
2026-08-24 22:22:48 -0300

    feat: Tratamento de erros

--- Servidor/Servidor.cpp
+++ Servidor/Servidor.cpp
@@ -8,15 +8,21 @@
 #include <unistd.h>
 
 
-// Função responsável por encontrar o valor de uma chave
-// dentro de um JSON simples.
-// Exemplo:
-// JSON:
-// {"tipo":"int","val":"25"}
-// chave:
-// "val"
-// Retorno:
-// "25"
+/*
+ * Função responsável por encontrar o valor de uma chave
+ * dentro de um JSON simples.
+ *
+ * Exemplo:
+ *
+ * JSON:
+ * {"tipo":"int","val":"25"}
+ *
+ * chave:
+ * "val"
+ *
+ * Retorno:
+ * "25"
+ */
 std::string pegarValor(
     const std::string& json,
     const std::string& chave
@@ -40,20 +46,30 @@ std::string pegarValor(
     return json.substr(inicio, fim - inicio);
 }
 
+
 int main() {
 
     sockaddr_in serverAddress{};
-    int serverSocket, clientSocket;
+    int serverSocket;
+    int clientSocket;
     char buffer[1024];
 
-    serverSocket = socket(AF_INET, SOCK_STREAM, 0);
+
+    serverSocket = socket(
+        AF_INET,
+        SOCK_STREAM,
+        0
+    );
+
 
     serverAddress.sin_family = AF_INET;
     serverAddress.sin_port = htons(8080);
     serverAddress.sin_addr.s_addr = INADDR_ANY;
 
+
     std::cout << "Servidor iniciando..." << std::endl;
     bind(serverSocket, (struct sockaddr*)&serverAddress, sizeof(serverAddress));
+
 
     std::cout << "Ouvindo em 127.0.0.1:8080" << std::endl;
     listen(serverSocket, 5);
@@ -74,22 +90,27 @@ int main() {
                 0
             );
 
+            if (bytesReceived == 0) {
+
+                std::cout << "Cliente desconectado." << std::endl;
+
+                break;
+
             buffer[bytesReceived] = '\0';
 
-            std::cout << "Mensagem do cliente: " << buffer << std::endl;
 
+            std::cout << "Mensagem do cliente: "<< buffer << std::endl;
             std::string mensagem(buffer);
-
             std::string tipo = pegarValor(mensagem, "tipo");
-
             std::string valor = pegarValor(mensagem, "val");
-
             std::string resposta;
 
             if (tipo == "int") {
 
                 int numero = std::stoi(valor);
+
                 numero++;
+
                 resposta = std::to_string(numero);
             }
 
@@ -106,8 +127,8 @@ int main() {
                         caractere = std::toupper(
                             static_cast<unsigned char>(caractere)
                         );
-
                     }
+
                     else if (std::isupper(
                         static_cast<unsigned char>(caractere)
                     )) {
@@ -116,6 +137,7 @@ int main() {
                             static_cast<unsigned char>(caractere)
                         );
                     }
+
 
                     resposta = caractere;
                 }
@@ -138,14 +160,15 @@ int main() {
 
             send(clientSocket, respostaJson.c_str(), respostaJson.size(), 0);
 
-            std::cout << "Resposta enviada: " << respostaJson;
+
+            std::cout << "Resposta enviada: "<< respostaJson;
         }
 
         close(clientSocket);
-        std::cout << "Cliente desconectado" << std::endl;
-    }
 
+    }
     close(serverSocket);
 
     return 0;
+    }
 }
